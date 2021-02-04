200c05f81684b0199cff9d3286910c57e383fd1c
Kevin Gaddy <kevin.gaddy@gmail.com>
2021-02-04 08:16:30 -0600

    Update serverMonitor.py
    Missing bracket!

--- serverMonitor.py
+++ serverMonitor.py
@@ -2,7 +2,7 @@ import os,signal
 from blinkt import clear,set_brightness, set_pixel, show,set_all
 from time import sleep
 
-hostlist = ["192.168.1.1", "192.168.1.2", "192.168.1.3", "192.168.1.4", "192.168.1.5", "192.168.1.6", "192.168.1.7", "8.8.8.8"
+hostlist = ["192.168.1.1", "192.168.1.2", "192.168.1.3", "192.168.1.4", "192.168.1.5", "192.168.1.6", "192.168.1.7", "8.8.8.8"]
 hostid = ["Device1", "Device2", "Device3", "Device4", "Device5", "Device6", "Device7", "Device8"]
 
 clear()
